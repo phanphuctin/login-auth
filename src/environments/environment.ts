68a9e48fdd2d80d42a6ebcc547f756ad4fb8ded6
phanphuctin <phanphuctin21@gmail.com>
2023-09-30 11:22:02 +0700

    "Install firebase"

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-auth-1f311',
+    appId: '1:965962470495:web:1a5a906106c1fdad03f7ea',
+    databaseURL: 'https://login-auth-1f311-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'login-auth-1f311.appspot.com',
+    apiKey: 'AIzaSyDussCwbgBaEy0JfFV2QWJOEVPOxf6ODFE',
+    authDomain: 'login-auth-1f311.firebaseapp.com',
+    messagingSenderId: '965962470495',
+    measurementId: 'G-RD2BVM9F1K',
+  },
+};
